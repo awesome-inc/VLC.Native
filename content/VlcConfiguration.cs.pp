b9389d42314f3b40da4f7d6909b6709b98909702
mkoertgen <marcel.koertgen@gmail.com>
2020-01-29 14:14:17 +0100

    Minor formatting & cleanup

--- content/VlcConfiguration.cs.pp
+++ content/VlcConfiguration.cs.pp
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using LibVlcWrapper;
 using Microsoft.Win32;
 
 namespace $rootnamespace$
@@ -12,13 +13,20 @@ namespace $rootnamespace$
     /// </summary>
     public class VlcConfiguration
     {
+        private const string UserEnvRegKey = @"HKEY_CURRENT_USER\Environment";
+        private const string SystemEnvRegKey =
+            @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment";
+
+        private readonly string _homePath;
         private string _libVlcVersion;
         private bool _vlcChecked;
-        private readonly string _homePath;
+
+        public Lazy<bool> IsVlcPresent { get; }
 
         public VlcConfiguration(string homePath = null)
         {
             _homePath = homePath ?? GetDefaultHomePath();
+            IsVlcPresent = new Lazy<bool>(InitVlc);
         }
 
         private string LibVlcVersion
@@ -27,59 +35,61 @@ namespace $rootnamespace$
             {
                 if (_libVlcVersion != null) return _libVlcVersion;
                 // This uses nVLC, cf.: https://www.nuget.org/packages/nVLC/
-                var pStr = LibVlcWrapper.LibVlcMethods.libvlc_get_version();
+                var pStr = LibVlcMethods.libvlc_get_version();
                 _libVlcVersion = Marshal.PtrToStringAnsi(pStr);
                 return _libVlcVersion;
             }
         }
 
-        private bool IsVlcPresent { get; set; }
-
         public void VerifyVlcPresent()
         {
-            if (!_vlcChecked)
-                CheckVlc();
-
-            if (!IsVlcPresent)
+            if (!IsVlcPresent.Value)
                 throw new InvalidOperationException("Cannot find VLC directory.");
         }
 
-        void CheckVlc()
+        private bool InitVlc()
         {
             var vlcPath = Path.Combine(_homePath, "vlc");
             var nativePath = Path.Combine(vlcPath, GetPlatform());
 
-            IsVlcPresent = Directory.Exists(nativePath);
+            var isVlcPresent = Directory.Exists(nativePath);
             if (!IsVlcPresent)
             {
                 Trace.TraceWarning("Cannot find VLC directory.");
-                return;
+                return false;
             }
 
-            // Prepend native path to environment path, to ensure the right libs are being used.
-            var path = Environment.GetEnvironmentVariable("PATH");
-            path = string.Concat(nativePath, ";", path);
-            Environment.SetEnvironmentVariable("PATH", path);
+            if (!_vlcChecked)
+            {
+                // Prepend native path to environment path, to ensure the right libs are being used.
+                var path = Environment.GetEnvironmentVariable("PATH");
+                path = string.Concat(nativePath, ";", path);
+                Environment.SetEnvironmentVariable("PATH", path);
 
-            Trace.TraceInformation("Using VLC {0} {1} from {2}",
-                LibVlcVersion, Environment.Is64BitProcess ? "x64" : "x86", nativePath);
+                Trace.TraceInformation("Using VLC {0} {1} from {2}",
+                    LibVlcVersion, Environment.Is64BitProcess ? "x64" : "x86", nativePath);
+            }
 
             _vlcChecked = true;
+            return true;
         }
 
         private static string GetDefaultHomePath()
         {
             var homePath = GetSafeEnv("VLC_HOME");
-            if (!string.IsNullOrWhiteSpace(homePath)) return homePath;
-            var executingAssemblyFile = new Uri(Assembly.GetEntryAssembly().GetName().CodeBase).LocalPath;
+            if (!string.IsNullOrWhiteSpace(homePath))
+            return homePath;
+
+            var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetCallingAssembly();
+            var executingAssemblyFile = new Uri(assembly.GetName().CodeBase).LocalPath;
             homePath = Path.GetDirectoryName(executingAssemblyFile);
             return homePath;
         }
 
-        private static string GetPlatform() { return Environment.Is64BitProcess ? "x64" : "x86"; }
-
-        private const string UserEnvRegKey = @"HKEY_CURRENT_USER\Environment";
-        private const string SystemEnvRegKey = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment";
+        private static string GetPlatform()
+        {
+            return Environment.Is64BitProcess ? "x64" : "x86";
+        }
 
         private static string GetSafeEnv(string envVar)
         {
